72e3ccc82e67b98c98c8f9a4a155a79a5d853ae5
TechCoderYorktown <156856447+TechCoderYorktown@users.noreply.github.com>
2024-06-15 23:12:39 -0400

    Update sign-in.js

--- sign-in.js
+++ sign-in.js
@@ -1,10 +1,12 @@
-function setFormMessage(formElement, type, message) {
-  const messageElement = formElement.querySelector(".form__message");
+document.addEventListener("DOMContentLoaded", () => {
+  const loginForm = document.querySelector("#login");
+  const createAccountForm = document.querySelector("#createAccount");
 
-  messageElement.textContent = message;
-  messageElement.classList.remove("form__message--success", "form__message--error");
-  messageElement.classList.add(`form__message--${type}`);
-}
+  document.querySelector("#linkCreateAccount").addEventListener("click", (e) => {
+    e.preventDefault();
+    loginForm.classList.add("form--hidden");
+    createAccountForm.classList.remove("form--hidden");
+  });
 
   document.querySelector("#linkLogin").addEventListener("click", (e) => {
     e.preventDefault();
@@ -16,8 +18,8 @@ function setFormMessage(formElement, type, message) {
     e.preventDefault();
 
     // Extract user input values
-    const username = loginForm.querySelector("#username").value;
-    const password = loginForm.querySelector("#password").value;
+    const username = loginForm.querySelector("#Username").value;
+    const password = loginForm.querySelector("#Password").value;
 
     // Check if the input matches specific criteria
     if (username === "bob" && password === "1234") {
@@ -27,7 +29,5 @@ function setFormMessage(formElement, type, message) {
       // Simulate an error message
       setFormMessage(loginForm, "error", "Invalid username/password combination");
     }
-  });
-
   });
 });
