162084023efc6b48e47c3b36062903b68e4d80f7
TechCoderYorktown <156856447+TechCoderYorktown@users.noreply.github.com>
2024-06-15 22:16:29 -0400

    Update sign-in.js

--- sign-in.js
+++ sign-in.js
@@ -1,3 +1,11 @@
+function setFormMessage(formElement, type, message) {
+  const messageElement=formElement.querySelector(".form__message");
+
+  messageElement.textContent=message;
+  messageElement.classList.remove("form__message--success", "form__message--error");
+  message Element.classList.add('form__message--${type}');
+}
+
 document.addEventListener("DOMContentLoaded", () => {
   const loginForm = document.querySelector("#login");
   const createAccountForm = document.querySelector("#createAccount");
@@ -12,5 +20,12 @@ document.addEventListener("DOMContentLoaded", () => {
     e.preventDefault();
     createAccountForm.classList.add("form--hidden");
     loginForm.classList.remove("form--hidden");
+  });
+
+  loginForm.addEventListener("submit", e => {
+    e.preventDefault();
+    //Perform your stuff
+
+    setFormMessage(loginForm, "error", "Invalid username/password combination");
   });
 });
