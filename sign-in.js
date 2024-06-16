bf3445bb71d8f99ba0aac027e9ba959ad84ec395
TechCoderYorktown <156856447+TechCoderYorktown@users.noreply.github.com>
2024-06-15 22:50:07 -0400

    Update sign-in.js

--- sign-in.js
+++ sign-in.js
@@ -6,11 +6,6 @@ function setFormMessage(formElement, type, message) {
   messageElement.classList.add(`form__message--${type}`);
 }
 
-function setInputError(inputElement, message) {
-  inputElement.classList.add("form__input--error");
-  inputElement.parentElement.querySelector(".form__input-error-message").textContent = message;
-}
-
 document.addEventListener("DOMContentLoaded", () => {
   const loginForm = document.querySelector("#login");
   const createAccountForm = document.querySelector("#createAccount");
@@ -30,22 +25,21 @@ document.addEventListener("DOMContentLoaded", () => {
   loginForm.addEventListener("submit", (e) => {
     e.preventDefault();
 
-     // Extract user input values
+    // Extract user input values
     const username = loginForm.querySelector("#username").value;
     const password = loginForm.querySelector("#password").value;
 
     // Check if the input matches specific criteria
     if (username === "bob" && password === "1234") {
-      // Redirect to example.com if the criteria match
-      window.location.href = "techcoderyorktown.github.io/home.html";
+      // Redirect to home.html if the criteria match
+      window.location.href = "https://techcoderyorktown.github.io/home.html";
     } else {
-      // AJAX/Fetch request to the server (if needed)
       // Simulate an error message
-    setFormMessage(loginForm, "error", "Invalid username/password combination");
+      setFormMessage(loginForm, "error", "Invalid username/password combination");
+    }
   });
-});
 
-createAccountForm.addEventListener("submit", (e) => {
+  createAccountForm.addEventListener("submit", (e) => {
     e.preventDefault();
 
     // Extract user input values
@@ -55,10 +49,9 @@ createAccountForm.addEventListener("submit", (e) => {
 
     // Check if the input matches specific criteria
     if (username === "XX" && password === "XX") {
-      // Redirect to example.com if the criteria match
-      window.location.href = "techcoderyorktown.github.io/home.html";
+      // Redirect to home.html if the criteria match
+      window.location.href = "https://techcoderyorktown.github.io/home.html";
     } else {
-      // AJAX/Fetch request to the server (if needed)
       // Simulate success message
       setFormMessage(createAccountForm, "success", "Account created successfully");
     }
